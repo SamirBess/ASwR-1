dfe2d4190165a46eb43a990b17d19bafdf9e5331
SamirBess <99830147+SamirBess@users.noreply.github.com>
2022-04-24 18:32:25 +0200

    Create p1.sh

---
+++ p1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N mnist_rf
+#PBS -l select=1:ncpus=128,walltime=00:50:00
+#PBS -q qexp
+#PBS -e mnist_rf.e
+#PBS -o mnist_rf.o
+
+module load R
+echo "loaded R"
+
+# Fix for warnings from libfabric/1.12 bug
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
+
+# I want the number to divide 76 = 4*19
+time mpirun -np 4 Rscript p1.R --args 4 32
